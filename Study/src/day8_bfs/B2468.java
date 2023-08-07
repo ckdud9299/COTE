375f1a0d7a7bb85696f0c614228cc8dcb36777bf
ckdud9299 <58415917+ckdud9299@users.noreply.github.com>
2023-08-07 15:16:51 +0900

    Add files via upload

---
+++ Study/src/day8_bfs/B2468.java
@@ -0,0 +1,92 @@
+package day8_BFS;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class B2468 {
+    // 움직이는 X,Y 좌표 (상우하좌 순)
+    static int [] dx = {-1,0,1,0};
+    static int [] dy = {0,1,0,-1};
+    // 방문 기록 저장 배열
+    static boolean [][] visited;
+    //데이터 저장 배열
+    static int [][] arr;
+
+    static int N, count;
+    static int maxHeight=0;
+    static int maxArea = 0;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        // 크기 N 입력 받기  
+        N = Integer.parseInt(st.nextToken()); 
+
+        // 입력받은 크기의 2차원 배열 선언
+        arr = new int [N][N];
+
+        // 높이 정보 입력받기
+        for(int i=0; i<N; i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j=0; j<N; j++){
+                arr[i][j] = Integer.parseInt(st.nextToken());
+
+                // 최대 높이를 찾음 (for문 반복횟수 줄일 수 있음)
+                if(arr[i][j] > maxHeight){
+                    maxHeight = arr[i][j];
+                }
+            }
+        }
+
+        // 물 높이 0부터 최대 높이까지 증가시키면서 bfs를 이용해 최대 영역 개수 구하기
+        for(int h=0; h<=maxHeight; h++){
+            count = 0;
+
+            visited = new boolean[N][N]; // 방문 배열 초기화
+
+            for(int i=0; i<N; i++){
+                for(int j=0; j<N; j++){
+                    if(!visited[i][j] && arr[i][j] > h){ // 방문 X 이고 물 높이보다 높은 경우
+                        count++;
+                        BFS(i,j,h);
+                    }
+                }
+            }
+
+            // maxArea에 안전한 영역의 최대 개수 저장
+            if(count > maxArea){
+                maxArea = count;
+            }
+        }
+
+        System.out.println(maxArea);
+
+    }
+
+    private static void BFS(int i, int j, int h) {
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[] {i,j});
+
+        while(!queue.isEmpty()){
+            int now[] = queue.poll();
+            visited[i][j] = true;
+
+            for(int k=0; k<4; k++){
+                int nx = now[0] + dx[k];
+                int ny = now[1] + dy[k];
+
+                if(nx>=0 && nx<N && ny>=0 && ny<N ){
+                    if(arr[nx][ny] > h && !visited[nx][ny]){
+                        visited[nx][ny] = true;
+                        queue.add(new int[] {nx,ny});
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Study/src/day8_bfs/B4963.java
@@ -0,0 +1,79 @@
+package day8_BFS;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class B4963 {
+    // 움직이는 X,Y 좌표 (위에서부터 시계방향)
+    static int [] dx = {-1,-1,0,1,1,1,0,-1};
+    static int [] dy = {0,1,1,1,0,-1,-1,-1};
+    // 방문 기록 저장 배열
+    static boolean [][] visited;
+    //데이터 저장 배열
+    static int [][] arr;
+
+    static int w,h,count;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        w = Integer.parseInt(st.nextToken());
+        h = Integer.parseInt(st.nextToken());
+
+        while(w != 0 && h != 0){ // 마지막 입력 0 0 을 받을 때 까지 반복
+            arr = new int [h][w];
+            visited = new boolean[h][w];
+            count=0;
+
+            // 해당 테스트 케이스 입력받기
+            for(int i=0; i<h; i++){
+                st = new StringTokenizer(br.readLine());
+                for(int j=0; j<w; j++){
+                    arr[i][j] = Integer.parseInt(st.nextToken());
+                }
+            }
+
+            // BFS를 통해서 섬의 개수 구하기
+            for(int i=0; i<h; i++){
+                for(int j=0; j<w; j++){
+                    if(!visited[i][j] && arr[i][j]==1){ // 방문 X 이고 1(섬)인 경우
+                        count++;
+                        BFS(i,j);
+                    }
+                }
+            }
+            System.out.println(count);
+
+            st = new StringTokenizer(br.readLine());
+            w = Integer.parseInt(st.nextToken());
+            h = Integer.parseInt(st.nextToken());
+        }
+
+    }
+
+    private static void BFS(int i, int j) {
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[] {i,j});
+
+        while(!queue.isEmpty()){ 
+            int now[] = queue.poll();
+            visited[i][j] = true;
+
+            for(int k=0; k<8; k++){ // k의 범위  
+                int nx = now[0] + dx[k];
+                int ny = now[1] + dy[k];
+
+                if(nx>=0 && nx<h && ny>=0 && ny<w ){
+                    if(arr[nx][ny] == 1 && !visited[nx][ny]){
+                        visited[nx][ny] = true;
+                        queue.add(new int[] {nx,ny});
+                    }
+                }
+            }
+        }
+    }
+}
