83a4b80bf5c613990801c1bb6b23e26a27771bfa
ckdud9299 <58415917+ckdud9299@users.noreply.github.com>
2023-09-11 18:16:49 +0900

    Add files via upload

---
+++ Study/src/day10_greedy/B16953.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class B16953 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        long A = sc.nextLong();
+        long B = sc.nextLong();
+        int count = 1;
+
+       while(A != B){
+           if(A > B) {
+               System.out.println(-1);
+               return;
+           }
+
+           if(B%2 == 0){
+               B /= 2;
+           } else if(B%10 == 1) {
+               B /= 10;
+           } else{
+               System.out.println(-1);
+               return;
+           }
+
+           count++;
+       }
+
+        System.out.println(count);
+
+    }
+}
---
+++ Study/src/day10_greedy/B2839_2.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class B2839_2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int count = 0;
+
+        while(N>=0){
+            if(N%5 == 0 ){
+                count += N/5;
+                System.out.println(count);
+                return;
+            }
+            N -= 3;
+            count++;
+
+        }
+
+        System.out.println("-1");
+    }
+}
